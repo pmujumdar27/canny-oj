304760cc89b1d2dbab38fe9c1f91f373f8366aa1
Pushkar Mujumdar <pushkarm27@gmail.com>
2022-10-12 00:53:43 +0530

    cleanup

--- backend/judge_service/new_judge/judge.py
+++ backend/judge_service/new_judge/judge.py
@@ -36,16 +36,11 @@ def compare(test_output_path, user_output_string):
     test_op = test_op_file.read()
     test_op = test_op.strip()
     user_output_string = user_output_string.strip()
-    # print("Test OP: ", test_op)
-    # print("User OP: ", user_output_string)
     if test_op == user_output_string:
         return 'AC'
     return 'WA'
 
 if __name__=='__main__':
-    # import psycopg2
-    # from get_config import get_db_config
-    # from queries import update_submission_status
     conn = None
 
     retval = compile(SUBMISSIONS_DIRECTORY + sys.argv[1], 'Cpp')
@@ -72,22 +67,3 @@ if __name__=='__main__':
     else:
         verdict = 'CE'
     print(verdict)
-
-    # try:
-    #     db_config = get_db_config()
-    #     print("DB Config: ", db_config)
-
-    #     print('Connecting to the PostgreSQL database...')
-    #     conn = psycopg2.connect(**db_config)
-        
-    #     cur = conn.cursor()
-
-    #     update_cnt = update_submission_status(cur, conn, sys.argv[4], verdict)
-    #     print("Updated: ", update_cnt)
-
-    # except (Exception, psycopg2.DatabaseError) as error:
-    #     print(error)
-    # finally:
-    #     if conn is not None:
-    #         conn.close()
-    #         print('Database connection closed!')
